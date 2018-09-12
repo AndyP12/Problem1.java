8541bd6a1c714eb7ad87294d29d0ff8187db1e5b
Andrew Purcell <t00199548@365s.ittralee.ie>
2018-09-12 10:45:25 +0100

    Initial commit

---
+++ src/Problem1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Problem1 {
+    public static void main(String[] args) {
+
+        String forename, initial, surname;
+        float distance;
+        int money;
+
+        JOptionPane.showInputDialog("Please enter first name");
+
+    }
+}
